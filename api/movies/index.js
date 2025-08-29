dfcbc6eb8b5caff13df0a112952e931369030f31
evan <evanbonner99@gmail.com>
2025-08-29 22:13:32 +0100

    Authentication lab - exercise (genres)

--- api/movies/index.js
+++ api/movies/index.js
@@ -1,7 +1,7 @@
 import movieModel from "./movieModel";
 import asyncHandler from "express-async-handler";
 import express from "express";
-import { getUpcomingMovies } from "../tmdb-api";
+import { getUpcomingMovies, getGenres } from "../tmdb-api";
 
 const router = express.Router();
 
@@ -51,6 +51,14 @@ router.get(
   "/tmdb/upcoming",
   asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }),
+);
+
+router.get(
+  "/tmdb/genres",
+  asyncHandler(async (req, res) => {
+    const upcomingMovies = await getGenres();
     res.status(200).json(upcomingMovies);
   }),
 );
